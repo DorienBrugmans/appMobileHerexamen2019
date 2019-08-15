3024a00afd5ae4701df7da8f344df72c971ca3e1
Dorien Brugmans <dorien.brugmans@allphi.eu>
2019-08-15 19:40:47 +0200

    Initial commit

---
+++ src/main/java/com/pxl/dorienbrugmans/digibonss/dummy/DummyContent.java
@@ -0,0 +1,68 @@
+package com.pxl.dorienbrugmans.digibonss.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    public static final List<DummyItem> CUSTOMERS = new ArrayList<DummyItem>();
+
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static {
+        // Add some sample items.
+        addItem(createDummyItem(1, "Person 1", "TestStreet 1, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(2, "Person 2", "TestStreet 2, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(3, "Person 3", "TestStreet 3, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(4, "Person 4", "TestStreet 4, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(5, "Person 5", "TestStreet 5, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(6, "Person 6", "TestStreet 6, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(7, "Person 7", "TestStreet 7, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(8, "Person 8", "TestStreet 8, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(9, "Person 9", "TestStreet 9, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(10, "Person 10", "TestStreet 10, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(11, "Person 11", "TestStreet 11, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(12, "Person 12", "TestStreet 12, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(13, "Person 13", "TestStreet 13, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(14, "Person 14", "TestStreet 14, 3500 Hasselt", "0123456789" ));
+        addItem(createDummyItem(15, "Person 15", "TestStreet 15, 3500 Hasselt", "0123456789" ));
+
+    }
+
+    private static void addItem(DummyItem item) {
+        CUSTOMERS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static DummyItem createDummyItem(int position, String name, String address, String phone) {
+        return new DummyItem(String.valueOf(position), name, address, phone);
+    }
+
+
+    public static class DummyItem {
+        public final String id;
+        public final String name;
+        public final String address;
+        public final String phone;
+
+        public DummyItem(String id, String name, String address, String phone) {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "Name: " + name + "\nAddress: " + address+ "\nPhone: " + phone;
+        }
+    }
+}
---
+++ src/main/java/com/pxl/dorienbrugmans/digibonss/utilities/NetworkUtils.java
@@ -0,0 +1,58 @@
+package com.pxl.dorienbrugmans.digibonss.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+
+public class NetworkUtils {
+    final static String GITHUB_BASE_URL =
+            "https://raw.githubusercontent.com/DorienBrugmans/MobileApp2019/master/Customers.json";
+
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.e("error build url", "buildUrl: " + e.getMessage());
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
